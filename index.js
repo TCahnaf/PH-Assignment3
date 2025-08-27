1388681566adb8b758052ec82100b516d8a4abbe
TCahnaf <ahnafchwd164@gmail.com>
2025-08-27 18:16:51 -0400

    finished layout and added most of the functionalities.

---
+++ index.js
@@ -0,0 +1,89 @@
+
+
+function captureElementID(id){
+    return document.getElementById(id);
+}
+
+function numberConvertedElementID(id){
+    const element = document.getElementById(id);
+
+    return parseInt(element.innerText);
+}
+
+function captureElementClasses(className){
+    return document.getElementsByClassName(className);
+}
+
+
+
+for(const element of captureElementClasses("heart-btn")){
+    element.addEventListener('click', function(){
+      let heartCount = numberConvertedElementID("heart-count");
+        console.log(heartCount);
+        heartCount += 1;
+    captureElementID("heart-count").innerText = heartCount;
+
+    })
+    
+}
+
+for(const element of captureElementClasses("call-btn")){
+    element.addEventListener('click', function(e){
+        let coinCount = numberConvertedElementID("coin-count");
+        coinCount -= 20;
+        const grandparent = e.target.parentElement.parentElement;
+        const serviceName = grandparent.getElementsByTagName("h1")[0].innerText;
+        const serviceNumber = grandparent.getElementsByTagName("h1")[1].innerText;
+     
+        if(coinCount < 0){
+            alert("OOPS !!! SORRY YOU ARE OUT OF COINS !!!")
+            return
+
+        }
+        else{
+            alert(`\u{1F4DE} Calling ${serviceName} ${serviceNumber}`)
+        }
+        captureElementID("coin-count").innerText = coinCount;
+        
+        const historyLog = captureElementID("history-log");
+        
+        const timeStamp = new Date()
+  .toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: true })
+  .toLowerCase();
+
+
+        let logContent = document.createElement("div");
+        logContent.innerHTML = `
+
+        <div class = "flex justify-between items-start gap-10 bg-[#FAFAFA] h-[100px] w-[450px] p-4">
+        <div>
+        <h2 class = "font-bold text-lg">${serviceName}</h2>
+        <h2>${serviceNumber}</h2>
+        </div>
+        <p>${timeStamp}</p>
+        </div>
+        `
+
+        historyLog.appendChild(logContent);
+        document.getElementById("clear-button").addEventListener('click', function(){
+            logContent.innerHTML = "";
+
+})
+
+       
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
